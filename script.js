549013d388a7e63e68e5c8b59aab2f941163644c
FafnirGefnar <167507292+FafnirGefnar@users.noreply.github.com>
2024-05-16 21:41:37 +0300

    add teretus function, syntax fixes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-console.log("tere");
-console.log("tere");
-
+//console.log("tere");
+//console.log("tere");
 
 //confirm("Today is sunny.");
 //prompt("What is your name?");
@@ -8,6 +7,16 @@ console.log("tere");
 
 //nimi = prompt("Mis on sinu nimi?")
 //vanus = prompt("Kui vana sa oled?");
+
+let klient = {
+    nimi:prompt("Mis on sinu nimi?"),
+    vanus:prompt("Kui vana sa oled?"),
+}
+
+function teretus(nimi) {
+    alert("Tere, " + nimi + "!")
+}
+teretus(klient.nimi)
 
 
 let toidud = ["banaan", "juustuburger", "mullivesi"]
@@ -19,10 +28,13 @@ for (let index = 0; index < toidud.length; index++) {
     
 }
 
-var kalaSoov = confirm("Kas sa kala sööd? ")
-
+var kalaSoov = confirm(klient.nimi + ", kas sa kala sööd?")
 if (kalaSoov) {
     toidud.push("sushi")
 }
-
 console.log(toidud);
+
+function randomToit(toit) {
+    console.log(toidud[toit]);
+}
+
