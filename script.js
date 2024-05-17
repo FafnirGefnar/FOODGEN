088e391e06219a831e128699c4fd33d1b267beb2
FafnirGefnar <167507292+FafnirGefnar@users.noreply.github.com>
2024-05-17 11:52:04 +0300

    finish random food generator and display on center of screen

--- script.js
+++ script.js
@@ -8,33 +8,37 @@
 //nimi = prompt("Mis on sinu nimi?")
 //vanus = prompt("Kui vana sa oled?");
 
-let klient = {
-    nimi:prompt("Mis on sinu nimi?"),
-    vanus:prompt("Kui vana sa oled?"),
+// let klient = {
+//     nimi:prompt("Mis on sinu nimi?"),
+//     vanus:prompt("Kui vana sa oled?"),
+// }
+
+// function teretus(nimi) {
+//     alert("Tere, " + nimi + "!")
+// }
+// teretus(klient.nimi)
+
+
+let toidud = ["Kanasalat", "Singipitsa", "Juustuburger", "Tortilla kebab", "Tomatipasta", "Kalavõileib", "Suitsukala", "Nigiri sushi", "Caesari salat", "Kebab friikartulitega", "Ahjuforell", "Lasanje", "Krevetisupp", "Tomatisupp", "Kana ja riis", "Veise antrekoot"];
+
+// for (let index = 0; index < toidud.length; index++) {
+//     //const element = array[index];
+//     console.log(toidud[index]);  
+// }
+
+// var kalaSoov = confirm(klient.nimi + ", kas sa kala sööd?");
+
+// if (kalaSoov) {
+//     toidud.push("sushi");
+// }
+
+function randomToit(list) {
+    let randInt = Math.floor(Math.random()*list.length);
+    return(list[randInt]);
 }
 
-function teretus(nimi) {
-    alert("Tere, " + nimi + "!")
-}
-teretus(klient.nimi)
+toit = randomToit(toidud)
+console.log("Sinu tänane toit: " + toit);
 
-
-let toidud = ["banaan", "juustuburger", "mullivesi"]
-console.log(toidud);
-
-for (let index = 0; index < toidud.length; index++) {
-    //const element = array[index];
-    console.log(toidud[index]);
-    
-}
-
-var kalaSoov = confirm(klient.nimi + ", kas sa kala sööd?")
-if (kalaSoov) {
-    toidud.push("sushi")
-}
-console.log(toidud);
-
-function randomToit(toit) {
-    console.log(toidud[toit]);
-}
-
+// document.addEventListener
+document.querySelector("#toit").innerHTML = "Sinu tänane toit: " + toit;
